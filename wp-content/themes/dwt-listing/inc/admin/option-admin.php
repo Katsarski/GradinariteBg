fa44637887e653542a9cbe7725b97b6e2e306354
Katsarski <boyankatsarski@gmail.com>
2020-07-06 22:24:07 +0300

    Initial commit

---
+++ wp-content/themes/dwt-listing/inc/admin/option-admin.php
@@ -0,0 +1,69 @@
+<?php
+
+if (!defined('ABSPATH'))
+    exit;
+/* ------------------Blog Settings ----------------------- */
+Redux::setSection($opt_name, array(
+    'title' => esc_html__('Admin Options', 'dwt-listing'),
+    'id' => 'dwt_listing_slugs',
+    'desc' => '',
+    'icon' => 'el el-asl',
+    'fields' => array(
+        array(
+            'id' => 'dwt_listing_admin_bar_switch',
+            'type' => 'switch',
+            'title' => esc_html__('Hide/Show Admin Bar', 'dwt-listing'),
+            'default' => true,
+        ),
+        array(
+            'id' => 'dwt_listing_enable_packages',
+            'type' => 'switch',
+            'title' => esc_html__('Assign Free Package On Registration', 'dwt-listing'),
+            'default' => true,
+        ),
+        array(
+            'id' => 'dwt_listing_bult_renew_date',
+            'type' => 'switch',
+            'title' => esc_html__('Renew listings in bulk', 'dwt-listing'),
+            'default' => false,
+        ),
+        array(
+            'id' => 'dwt_listing_notification_packages',
+            'type' => 'info',
+            'title' => esc_html__('Package Alert', 'dwt-listing'),
+            'style' => 'critical',
+            'desc' => esc_html__('This option will only work when ( WooCommerce Plugin ) is activeted & ( Downtown Packages ) are created.', 'dwt-listing'),
+            'required' => array('dwt_listing_enable_packages', '=', true),
+        ),
+        array(
+            'id' => 'dwt_listing_package_type',
+            'type' => 'select',
+            'data' => 'post',
+            'args' => array(
+                'post_type' => array('product'),
+                'post_status' => 'publish',
+                'posts_per_page' => -1,
+                'order' => 'DESC',
+                'orderby' => 'date',
+            ),
+            'title' => esc_html__('Select Package Only', 'dwt-listing'),
+            'required' => array('dwt_listing_enable_packages', '=', true),
+            'desc' => esc_html__('Only select package not shop products.', 'dwt-listing'),
+        ),
+        array(
+            'id' => 'dwt_listing_disable_menu',
+            'type' => 'switch',
+            'title' => esc_html__('Enable Menu', 'dwt-listing'),
+            'default' => true,
+        ),
+        array(
+            'id' => 'dwt_listing_brand_default_image',
+            'type' => 'media',
+            'url' => true,
+            'title' => __('Default Brand Image', 'dwt-listing'),
+            'compiler' => 'true',
+            'desc' => __('Select brand default image here', 'dwt-listing'),
+            'default' => array('url' => trailingslashit(get_template_directory_uri()) . 'assets/images/brand.png'),
+        ),
+    )
+));
